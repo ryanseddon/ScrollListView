323ab6eccb62585303d60c5d19408fef6220ad18
Ryan Seddon <seddon.ryan@gmail.com>
2014-02-28 14:54:48 +1100

    Massive refactor
    * Improved logic around handling scroll direction
    * Hugely simplified figuring out which cell to change yay modulus
    * Have code to only fire off one rAF at a time but it's sloooow in FF
    * cell ordering is now consistent up or down

--- ScrollListView.js
+++ ScrollListView.js
@@ -1,119 +1,113 @@
-//(function(window, document){
+//function(window, document){
 
-    var winHeight = window.innerHeight,
-        body = document.body,
-        slice = [].slice,
-        CELLHEIGHT = 150,
-        listContainer = document.createElement('ul'),
-        listContainerStyle = listContainer.style,
-        cells,
-        cellsWithinViewportCount = Math.ceil(winHeight/CELLHEIGHT) * 2,
-        cellsFrag = document.createDocumentFragment(),
-        scrollTimer,
-        rAF,
-        cellsOutOfViewportCount = 0,
-        pageCount = 20,
-        scrollPos = 1,
-        isScrollingDown,
-        orderProp = Modernizr.prefixed('order');
-        scrollTop = lastScrollTop = body.scrollTop,
-        direction = 0,
-        cellsState = {},
-        isTopCellOutOfView = false,
-        currentCell = null,
-        ticking = false;
+var winHeight = window.innerHeight,
+body = document.body,
+slice = [].slice,
+CELLHEIGHT = 150,
+listContainer = document.createElement('ul'),
+listContainerStyle = listContainer.style,
+cells,
+cellsWithinViewportCount = Math.ceil(winHeight/CELLHEIGHT) * 2,
+cellsFrag = document.createDocumentFragment(),
+scrollTimer,
+rAF,
+cellsOutOfViewportCount = cellsWithinViewportCount,
+pageCount = 20,
+scrollPos = 1,
+isScrollingDown,
+orderProp = Modernizr.prefixed('order');
+scrollTop = lastScrollTop = body.scrollTop,
+direction = 0,
+cellsState = {},
+isTopCellOutOfView = false,
+currentCell = null,
+ticking = false;
 
-    rAF = Modernizr.prefixed('requestAnimationFrame', window) || function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-    };
+rAF = Modernizr.prefixed('requestAnimationFrame', window) || function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+};
 
-    function isTopElementOutOfViewport(el) {
-        var elemPostion = el.getBoundingClientRect();
+function isTopElementOutOfViewport(el) {
+    var elemPostion = el.getBoundingClientRect();
 
-        return !!elemPostion && elemPostion.bottom <= -CELLHEIGHT; //(CELLHEIGHT*2);
+    return !!elemPostion && elemPostion.bottom <= -(CELLHEIGHT*2);
+}
+
+function isBottomElementOutOfViewport(el) {
+    var elemPostion = el.getBoundingClientRect();
+
+    return !!elemPostion && elemPostion.top > winHeight + (CELLHEIGHT*2);
+}
+
+function onScroll() {
+    clearTimeout(timer);
+    scrollTop = body.scrollTop;
+    direction = scrollTop - lastScrollTop;
+    //requestTick();
+    checkCells();
+    var timer = setTimeout(function(){
+        checkCells();
+    },200);
+}
+
+function requestTick() {
+    if(!ticking) {
+        rAF(checkCells);
+        ticking = true;
     }
+}
 
-    function isBottomElementOutOfViewport(el) {
-        var elemPostion = el.getBoundingClientRect();
+function getCurrentCell(count) {
+    return cells[count % cells.length];
+}
 
-        return !!elemPostion && elemPostion.top > winHeight + CELLHEIGHT; //(CELLHEIGHT*2);
-    }
+function checkCells() {
+    cells = cells || slice.call(listContainer.children);
+    isScrollingDown = direction > 0;
+    currentCell = getCurrentCell(cellsOutOfViewportCount);
 
-    function onScroll() {
-        scrollTop = body.scrollTop;
-        direction = scrollTop - lastScrollTop;
-        requestTick();
-    }
+    if(isScrollingDown) {
+        isTopCellOutOfView = isTopElementOutOfViewport(currentCell);
 
-    function requestTick() {
-        if(!ticking) {
-            rAF(checkCells);
-            ticking = true;
+        if(isTopCellOutOfView) {
+            cellsOutOfViewportCount++;
+            cellsState.index = cellsOutOfViewportCount;
+            listContainerStyle.paddingTop = parseInt(listContainerStyle.paddingTop || 0, 10) + CELLHEIGHT + 'px';
+            currentCell.style[orderProp] = cellsState.index;
+            currentCell.innerHTML = tmpl('tweet_tpl', tweets[cellsState.index-1]);
         }
-    }
+    } else if(!isScrollingDown) {
+        currentCell = getCurrentCell(cellsOutOfViewportCount-1);
+        isBottomCellOutOfView = isBottomElementOutOfViewport(currentCell);
 
-    function getCurrentCell() {
-        return cells[cellsOutOfViewportCount % cells.length];
-    }
-
-    function getCurrentBottomCell() {
-        var index = cellsOutOfViewportCount-1 % cells.length;
-
-        if(index < 0 ) {
-
-        }
-        return cells[index];
-    }
-
-    function checkCells() {
-        cells = cells || slice.call(listContainer.children);
-        isScrollingDown = direction > 0;
-        currentCell = getCurrentCell();
-        
-
-        if(isScrollingDown) {
-            isTopCellOutOfView = isTopElementOutOfViewport(currentCell);
-
-            if(isTopCellOutOfView) {
-                cellsOutOfViewportCount++;
-                cellsState.index = cells.length + cellsOutOfViewportCount;
-                listContainerStyle.paddingTop = parseInt(listContainerStyle.paddingTop || 0, 10) + CELLHEIGHT + 'px';
-                currentCell.style[orderProp] = cellsState.index;
-                currentCell.innerHTML = tmpl('tweet_tpl', tweets[cellsState.index]);
-            }
-        } else {
-            currentCell = getCurrentBottomCell();
-            console.log('order: ', currentCell.style[orderProp]);
-            isBottomCellOutOfView = isBottomElementOutOfViewport(currentCell);
-
-            if(isBottomCellOutOfView) {
-                cellsOutOfViewportCount--;
-                cellsState.index = cells.length + cellsOutOfViewportCount;
-                listContainerStyle.paddingTop = parseInt(listContainerStyle.paddingTop || 0, 10) - CELLHEIGHT + 'px';
-                currentCell.style[orderProp] = cellsState.index;
-                currentCell.innerHTML = tmpl('tweet_tpl', tweets[cellsState.index]);
-            }
-
+        if(isBottomCellOutOfView && cellsOutOfViewportCount > cellsWithinViewportCount) {
+            cellsState.index = cells[cellsOutOfViewportCount-- % cells.length].style[orderProp] - 1;
+            listContainerStyle.paddingTop = parseInt(listContainerStyle.paddingTop || 0, 10) - CELLHEIGHT + 'px';
+            currentCell.style[orderProp] = cellsState.index;
+            currentCell.innerHTML = tmpl('tweet_tpl', tweets[cellsState.index-1]);
         }
 
-        lastScrollTop = body.scrollTop;
-        ticking = false;
     }
 
-    for(var i = 0; i < cellsWithinViewportCount; i++) {
-        var cell = document.createElement('li'),
-            tweet = tweets[i];
+    lastScrollTop = body.scrollTop;
+    ticking = false;
+}
 
-        cell.className = 'scrolllist__cell  gpuarise';
-        cell.innerHTML = tmpl('tweet_tpl', tweet);
-        cellsFrag.appendChild(cell);
-    }
+for(var i = 0; i < cellsWithinViewportCount; i++) {
+    var cell = document.createElement('li'),
+    tweet = tweets[i];
 
-    listContainer.className = 'scrolllist gpuarise';
-    listContainerStyle.minHeight = tweets.length * CELLHEIGHT + 'px';
-    listContainer.appendChild(cellsFrag);
-    body.appendChild(listContainer);
+    cell.className = 'scrolllist__cell  gpuarise';
+    cell.innerHTML = tmpl('tweet_tpl', tweet);
+    cell.style[orderProp] = i+1;
+    cellsFrag.appendChild(cell);
+}
 
-    window.addEventListener('scroll', onScroll, false);
+listContainer.className = 'scrolllist gpuarise';
+listContainerStyle.minHeight = tweets.length * CELLHEIGHT + 'px';
+listContainer.appendChild(cellsFrag);
+body.appendChild(listContainer);
 
-//}(this, this.document));
+window.addEventListener('scroll', onScroll, false);
+
+//}(this, this.document);
